6aa41075ec13568c8ad1065f2a2474a40a2427fe
Sebastian Falborg <sb@ecreo.dk>
2023-03-10 14:26:55 +0100

    Update test data

--- EcreoFootballApi/Controllers/MatchController.cs
+++ EcreoFootballApi/Controllers/MatchController.cs
@@ -83,37 +83,37 @@ public class MatchController : ControllerBase
             }
         };
         
-        // var match2 = new Match
-        // {
-        //     Date = DateTime.Parse("10/03/2023 14:00"),
-        //     Games = new List<Game>
-        //     {
-        //         new()
-        //         {
-        //             Order = 1,
-        //             BlueTeam = teamA,
-        //             BlueTeamScore = 10,
-        //             RedTeam = teamB,
-        //             RedTeamScore = 5
-        //         },
-        //         new()
-        //         {
-        //             Order = 2,
-        //             BlueTeam = teamA,
-        //             BlueTeamScore = 10,
-        //             RedTeam = teamB,
-        //             RedTeamScore = 8
-        //         },
-        //         new()
-        //         {
-        //             Order = 3,
-        //             BlueTeam = teamA,
-        //             BlueTeamScore = 7,
-        //             RedTeam = teamB,
-        //             RedTeamScore = 10
-        //         },
-        //     }
-        // };
+        var match2 = new Match
+        {
+            Date = DateTime.Parse("10/03/2023 14:00"),
+            Games = new List<Game>
+            {
+                new()
+                {
+                    Order = 1,
+                    BlueTeam = teamA,
+                    BlueTeamScore = 3,
+                    RedTeam = teamB,
+                    RedTeamScore = 10
+                },
+                new()
+                {
+                    Order = 2,
+                    BlueTeam = teamA,
+                    BlueTeamScore = 10,
+                    RedTeam = teamB,
+                    RedTeamScore = 4
+                },
+                new()
+                {
+                    Order = 3,
+                    BlueTeam = teamA,
+                    BlueTeamScore = 5,
+                    RedTeam = teamB,
+                    RedTeamScore = 10
+                },
+            }
+        };
 
         return new List<Match> { match1 };
     }
